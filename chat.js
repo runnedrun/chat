89541f40b1c6111c519446fa1b94d12f0e50fb7e
David Gaynor <runnedrun@gmail.com>
2016-11-01 10:03:47 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -14,15 +14,9 @@ input.keydown(function(e) {
   if (e.keyCode == 13) {
     var message = input.val();
     var antijosh = message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var cleanerUser = (String(jackIsDumb))
-    var ccleanerUser = cleanerUser.replace(/</g, "&lt;").replace(/>/g, "&gt;")
-    if (ccleanerUser.length < 10) {
-      digitChecker = "t"
-    }
-    if (ccleanerUser.length > 9) {
-      digitChecker = "f"
-    }
-    var useUser = (ccleanerUser + ": " + antijosh + userLength + digitChecker)
+    var cleanerUser = (String(jackIsDumb));
+    var ccleanerUser = cleanerUser.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var useUser = (ccleanerUser + ": " + antijosh);
     addListItem("saylist", useUser);
     input.val("");
   }
@@ -31,16 +25,9 @@ input.keydown(function(e) {
 // Display when new things are added _______________________________
 
 onNewListItem("saylist", function(r) {
-  var reclean = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"))
-  var userDigit = (reclean.length -1)
-  if (userDigit == "t") {
-    var userNumber = (reclean.length - 2, reclean.length - 1);
-  }
-  if (userDigit == "f") {
-    var userNumber = (reclean.length - 3, reclean.length - 2);
-  }
-  var finalalmost2 = (reclean.length(0 - userNumber).bold().fontcolor(accountColor) + reclean.length(userNumber, reclean.length))
-  chatlist.push(finalalmost2)
+  var reclean = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
+  chatlist.push(reclean);
+  var workPlease = reclean.split(": ")
   var limit = chatlist.slice(chatlist.length - 20, chatlist.length);
   chat.html(limit.join("<br>"));
  })
