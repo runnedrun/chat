0e18d5fb888a7d79d58ae9b55242c7bce720f754
David Gaynor <runnedrun@gmail.com>
2016-11-01 16:30:22 -0700

    Classadoo autocommit

--- chat.js
+++ chat.js
@@ -17,7 +17,7 @@ input.keydown(function(e) {
     var cleanerUser = (String(jackIsDumb));
     var ccleanerUser = cleanerUser.replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var useUser = (ccleanerUser + ": " + antijosh);
-    addListItem("saylist", useUser);
+    addListItem("saylist", useUser + accountColor + accountColor.length);
     input.val("");
   }
 })
@@ -26,8 +26,13 @@ input.keydown(function(e) {
 
 onNewListItem("saylist", function(r) {
   var reclean = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
-  chatlist.push(reclean);
-  var workPlease = reclean.split(": ")
+  var getTheirColor1 = (reclean.slice(reclean.length-1))
+  var getTheirColor2 = (reclean.slice(reclean.length-getTheirColor1+1,reclean.length-1))
+  console.log(getTheirColor1)
+  console.log(getTheirColor2)
+  var workPlease = reclean.split(": ")[0]
+  var stylize = (workPlease.bold().fontcolor(accountColor) + ": " + reclean.split(": ")[1])
+  chatlist.push(stylize);
   var limit = chatlist.slice(chatlist.length - 20, chatlist.length);
   chat.html(limit.join("<br>"));
  })
