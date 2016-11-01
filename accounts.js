7b2937ee1828ae76b145804df861791741bf8c5d
David Gaynor <runnedrun@gmail.com>
2016-11-01 08:41:28 -0700

    Classadoo autocommit

--- accounts.js
+++ accounts.js
@@ -32,17 +32,25 @@ var pass2 = $("#passcheck");
 //On button press Create ---------------------------------
 
 create.click(function() {
+  var accountstyle = $("#color").val()
+  if (user.length < 11) {
   user = newUser.val();
+  }
+  else {
+    $("#toolong").css({display: "block"});
+  }
   if (pass1.val() == pass2.val()) {
     var newPass = pass1.val()
   }
     else {
-      $("#nomatch").css({display: "block"})
+      $("#nomatch").css({display: "block"});
     }
   storeValue(user,newPass)
+  storeValue(newPass, accountstyle)
   pass1.val("")
   pass2.val("")
   newUser.val("")
+  $("#color").val("")
 })
 
 //Sign In ----------------------------------------------------
@@ -51,14 +59,22 @@ var username = $("#username");
 
 var password = $("#password");
 
-var realUser = "anonymous user"
+var realUser = "Anonymous User"
 
 //Sign in session -------------------------------------------
 
-
+var signedin = 0
 var session = {
   name: realUser
 }
+
+//color------------------------------------------
+
+var accountColor = "black"
+
+//Wow! I'm an idiot------------------------------
+
+var jackIsDumb = "Anonymous User"
 
 //On button press Log in--------------
 
@@ -67,14 +83,20 @@ $("#signbutton").click(function() {
   $("#right").css({display: "none"})
    realUser = username.val()
   var realPass = password.val()
+  once(realPass, function(color) {
+    accountColor = color
+    console.log(accountColor);
+  })
   once(realUser, function(password) {
     if (password == realPass) {
       $("#right").css({display: "block"});
-      console.log(session.name)
+      signedin = 1
+     jackIsDumb = realUser
+     $("#topbar").html("You are signed in as " + jackIsDumb.fontcolor(accountColor));
     }
-      else {
-        $("#wrong").css({display: "block"});
-      }
+    else {
+      $("#wrong").css({display: "block"});
+    }
   })
 })
 
--- chat.js
+++ chat.js
@@ -3,6 +3,7 @@
 var input = $("#input")
 var chat = $("#lechat")
 
+var digitChecker = "f"
 // The Master List... ______________________________________________
 
 var chatlist = []
@@ -13,7 +14,15 @@ input.keydown(function(e) {
   if (e.keyCode == 13) {
     var message = input.val();
     var antijosh = message.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var useUser = (realUser + ": " + antijosh)
+    var cleanerUser = (String(jackIsDumb))
+    var ccleanerUser = cleanerUser.replace(/</g, "&lt;").replace(/>/g, "&gt;")
+    if (ccleanerUser.length < 10) {
+      digitChecker = "t"
+    }
+    if (ccleanerUser.length > 9) {
+      digitChecker = "f"
+    }
+    var useUser = (ccleanerUser + ": " + antijosh + userLength + digitChecker)
     addListItem("saylist", useUser);
     input.val("");
   }
@@ -22,8 +31,17 @@ input.keydown(function(e) {
 // Display when new things are added _______________________________
 
 onNewListItem("saylist", function(r) {
-  chatlist.push(r);
-  var limit = chatlist.slice(chatlist.length - 25, chatlist.length);
+  var reclean = (r.replace(/</g, "&lt;").replace(/>/g, "&gt;"))
+  var userDigit = (reclean.length -1)
+  if (userDigit == "t") {
+    var userNumber = (reclean.length - 2, reclean.length - 1);
+  }
+  if (userDigit == "f") {
+    var userNumber = (reclean.length - 3, reclean.length - 2);
+  }
+  var finalalmost2 = (reclean.length(0 - userNumber).bold().fontcolor(accountColor) + reclean.length(userNumber, reclean.length))
+  chatlist.push(finalalmost2)
+  var limit = chatlist.slice(chatlist.length - 20, chatlist.length);
   chat.html(limit.join("<br>"));
  })
 
